16b41649f7f21c3d673bbb23b17701c514abb429
Schwiftytv <30345560+Schwiftytv@users.noreply.github.com>
2017-07-21 14:35:33 -0500

    1.13

--- my-videos.php
+++
@@ -1,7 +0,0 @@
-<?php
-$myVideos = array(
-    '<iframe src="https://www.youtube.com/embed/jpCWEq5JIV0?rel=0&amp;controls=0&amp;showinfo=0" width="500" height="300"></iframe>',
-    '<iframe src="https://www.youtube.com/embed/jpCWEq5JIV0?rel=0&amp;controls=0&amp;showinfo=0" width="500" height="300"></iframe>',
-    '<iframe src="https://www.youtube.com/embed/jpCWEq5JIV0?rel=0&amp;controls=0&amp;showinfo=0" width="500" height="300"></iframe>'
-);
-?>
