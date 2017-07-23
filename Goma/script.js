1d0c00eb4b512809a1bd327e4e47e6fce1ce7816
Schwiftytv <30345560+Schwiftytv@users.noreply.github.com>
2017-07-23 06:20:45 -0500

    3.2

--- Goma/script.js
+++
@@ -1,8 +0,0 @@
-    jQuery(function(){
-        jQuery("a.bla-1").YouTubePopUp();
-        jQuery("a.bla-2").YouTubePopUp( { autoplay: 0 } ); // Disable autoplay
-    });
-	
-		function goBack() {
-    window.history.back();
-}
